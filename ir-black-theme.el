f3d2b6b0c18307c1ff59f28c8369b489dfeb5655
Balaji Sivaraman <sivaraman.balaji@gmail.com>
2013-08-30 19:38:47 +0530

    Removed underline from highlight and a few other things

--- ir-black-theme.el
+++ ir-black-theme.el
@@ -81,7 +81,7 @@
    `(header-line ((t (:background, *mode-line-bg* :foreground, *normal*)))) ;; info header
    `(highlight ((t (:background, *current-line*))))
    `(highlight-face ((t (:background, *current-line*))))
-   `(hl-line ((t (:background, *current-line* :underline t))))
+   `(hl-line ((t (:background, *current-line* ))))
    `(info-xref ((t (:foreground, *keywords* :underline t))))
    `(region ((t (:background, *visual-selection*))))
    `(underline ((nil (:underline t))))
